e9da1d5ba653fd673e5797ee1911a70827ac3438
Tim Bornemann <65897899+timbornemann@users.noreply.github.com>
2025-10-29 10:35:12 +0100

    Fix runtime API URL exposure for desktop

--- desktop/electron/preload.js
+++ desktop/electron/preload.js
@@ -4,6 +4,12 @@ const apiArgument = process.argv.find(arg => arg.startsWith('--apiBaseUrl='));
 const apiBaseUrl = apiArgument ? apiArgument.split('=')[1] : undefined;
 
 if (apiBaseUrl) {
+  try {
+    contextBridge.exposeInMainWorld('__CCC_API_URL__', apiBaseUrl);
+  } catch (error) {
+    console.warn('Unable to expose __CCC_API_URL__ via contextBridge:', error);
+  }
+
   window.__CCC_API_URL__ = apiBaseUrl;
 }
 
